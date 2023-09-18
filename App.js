36806c4090fe748c3f54763bb10eff11ff08d2b3
kaamyxx <kamilyxavierp@gmail.com>
2023-09-17 22:16:03 -0300

    commit5

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import List from './screens/List';
+import Form from './screens/Form';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="List" component={List} />
+        <Stack.Screen name="Form" component={Form} />
         <Stack.Screen name="Nasa" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
